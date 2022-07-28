bebc27c4fd973234a82851b0009259a4caeb7563
winni4eva <adams4lyf@gmail.com>
2022-07-28 01:04:01 +0000

    Added dashboard, offers and subscription components

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { OffersComponent } from './offers/offers.component';
+import { SubscriptionsComponent } from './subscriptions/subscriptions.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    DashboardComponent,
+    OffersComponent,
+    SubscriptionsComponent
+  ],
   imports: [
     CommonModule
   ]
